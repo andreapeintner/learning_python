c62872a26e84d84d981e5f2cc62e137c9470d1ea
andi <andreapeintner@gmail.com>
2017-07-11 15:53:53 +0200

    Add a longer time period

--- break.py
+++ break.py
@@ -6,6 +6,6 @@ break_count = 0
 
 print("This program starts at "+time.ctime())
 while (break_count < total_breaks ) :
-    time.sleep(60)
+    time.sleep(2*60*60)
     webbrowser.open("https://www.youtube.com/watch?v=DmG59XHj1T4")
     break_count = break_count + 1
